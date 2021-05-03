cb1e31af7e15cbb1419071a9912716c4dba3e703
PNJaenichen <PNJaenichen@gmail.com>
2021-05-02 21:41:01 -0400

    Working on doing async Promise to get game information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import getGame from "./components/getGame";
+
+function doSearch() {
+  const theThing = getGame();
+  console.log(theThing);
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
-      <p>Click the search link above. Make a choice for at least team 1 and the associated season.</p> 
-      <p>If a single team is selected, than all of that teams regular season games will be returned.</p> 
-      <p>If both teams are selected, than only games between those two teams will be returned.</p>
-      <p>Get the score, date, and arena for the games returned.</p>
+      <div>
+        <p>Click the search link above. Make a choice for at least team 1 and the associated season.</p> 
+        <p>If a single team is selected, than all of that teams regular season games will be returned.</p> 
+        <p>If both teams are selected, than only games between those two teams will be returned.</p>
+        <p>Get the score, date, and arena for the games returned.</p>
+      </div> 
+      <button onClick={doSearch}>Search</button>
+      
     </div>
   );
 }
